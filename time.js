3d09c50f2b28cebf562d450057a241896b602fc1
xHecker <91829843+xHecker@users.noreply.github.com>
2022-02-23 16:38:49 +0530

    launch presale

--- time.js
+++ time.js
@@ -1,14 +1,29 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Feb 24, 2022 01:30:00").getTime();
 
-// Update the count down every 1 second
+var d = new Date("Feb 24, 2022 01:30:00");
+
+var localTime = d.getTime();
+
+var localOffset = d.getTimezoneOffset() * 60000;
+
+var utc = localTime + localOffset;
+
+// obtain and add destination's UTC time offset
+// for example, Bombay 
+// which is UTC + 5.5 hours
+var offset = 5.5;
+var bombay = utc + (3600000 * offset);
+
+var nd = new Date(bombay);
+
 var x = setInterval(function() {
 
     // Get today's date and time
     var now = new Date().getTime();
 
     // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+    var distance = nd - now;
 
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
